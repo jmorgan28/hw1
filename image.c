b8c5e46b9145961c21b9f4aaa728175c4d5336e2
Jackson Morgan <jacksonmorgan@Jacksons-MacBook-Pro.local>
2017-02-04 23:36:09 -0500

    what

--- image.c
+++ image.c
@@ -1,6 +1,26 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <string.h>
 
 
 int main(){
+  int fd = open("firstimage.ppm", O_CREAT | O_WRONLY, 0644 );
+  write(fd, "P3 500 500 255\n", 15);
+  int i = 0;
+  int f = 0;
+  while(i < 500){
+    while(f < 500){
+      write(fd,"33",2);
+      write(fd,"94",2);
+      write(fd,"200",2);
+      f ++;
+    }
+    i ++;
+  }
+  close(fd);
   return 0; 
 }
