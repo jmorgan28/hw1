b89367a993620369fe40dd9fa2e54dcad13236b2
Jackson Morgan <jacksonmorgan@Jacksons-MacBook-Pro.local>
2017-02-05 11:57:23 -0500

    shape

--- image.c
+++ image.c
@@ -10,25 +10,35 @@
 int main(){
   char * store = (char *) malloc(25);
   int fd = open("firstimage.ppm", O_CREAT | O_WRONLY | O_TRUNC, 0644 );
-  sprintf(store, "P3 750 750 255\n");
+  sprintf(store, "P3 500 500 255\n");
   write(fd,store, strlen(store));
-  int i, f, r, g, b;
+  int i, f, r, g, b, bonus;
   r = 0;
   g = 0;
   b = 0;
-  for(i = 0; i < 750; i ++){
-    for(f = 0; f < 750; f ++){
-      r = 245;
-      g = 120;
-      b = 11;
-      sprintf(store, "%d %d %d\n", r,g,b);
+  bonus = 0;
+  for(i = 0; i < 500; i ++){
+    for(f = 0; f < 500; f ++){
+      if(f >= 250 - bonus && f <= 250 + bonus){
+	r = 0;
+	g = 0;
+	b = 255;
+      }
+      else{
+	r = 245;
+	g = 120;
+	b = 11;
+      }
+      sprintf(store, "%d %d %d ", r,g,b);
       write(fd,store,strlen(store));
-      f ++;
     }
-    i ++;
+    if(i <= 250){
+      bonus ++;
+    }
+    else{
+      bonus --;
+    }
   }
-  
-  
   close(fd);
   return 0; 
 }
